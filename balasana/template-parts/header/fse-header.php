3070f686823294dc5b1da2a0cf19580851216e16
Nazim Ali <nazimali@gmail.com>
2021-12-18 20:01:32 +0000

    m

--- balasana/template-parts/header/fse-header.php
+++
@@ -1,6 +0,0 @@
-<header id="masthead" class="site-header responsive-max-width">
-	<?php
-		$template = new A8C\FSE\WP_Template();
-		$template->output_template_content( A8C\FSE\WP_Template::HEADER );
-	?>
-</header>
